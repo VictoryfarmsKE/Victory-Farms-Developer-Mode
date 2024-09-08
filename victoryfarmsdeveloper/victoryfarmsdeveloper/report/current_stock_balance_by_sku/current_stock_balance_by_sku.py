bc80edd1dbfdaf414613c317790f6b1ab7cbc5b4
ChristineKanga-VF <christinek@victoryfarmskenya.com>
2024-09-08 14:04:49 +0300

    stock_balance-aggregate

--- victoryfarmsdeveloper/victoryfarmsdeveloper/report/current_stock_balance_by_sku/current_stock_balance_by_sku.py
+++ victoryfarmsdeveloper/victoryfarmsdeveloper/report/current_stock_balance_by_sku/current_stock_balance_by_sku.py
@@ -135,8 +135,8 @@ class StockBalanceReport:
 
 				report_data.update(stock_ageing_data)
 
-			# Group by item_name
-			key = report_data.get('item_code')  # or 'item_code' if you prefer
+			# Group by item_code
+			key = report_data.get('item_code')
 			if key not in grouped_data:
 				grouped_data[key] = {
 					"item_code": report_data.get("item_code"),
