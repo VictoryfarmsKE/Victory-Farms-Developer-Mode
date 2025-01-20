c25c7ae19937d1332f74eea4d3aca1c8b5e2a4a7
ChristineKanga-VF <christinek@victoryfarmskenya.com>
2025-01-20 20:17:16 +0300

    custom appraisal cycle

--- victoryfarmsdeveloper/hooks.py
+++ victoryfarmsdeveloper/hooks.py
@@ -131,8 +131,8 @@ doctype_js = {
 # 	"ToDo": "custom_app.overrides.CustomToDo"
 # }
 override_doctype_class = {
-    "Stock Entry": "victoryfarmsdeveloper.victoryfarmsdeveloper.customization.stock_entry.stock_entry.CustomStockEntry"
-    # "Appraisal": "victoryfarmsdeveloper.victoryfarmsdeveloper.customization.appraisal.appraisal.CustomAppraisal"
+    "Stock Entry": "victoryfarmsdeveloper.victoryfarmsdeveloper.customization.stock_entry.stock_entry.CustomStockEntry",
+    "Appraisal Cycle": "victoryfarmsdeveloper.victoryfarmsdeveloper.customization.appraisal_cycle.appraisal_cycle.CustomAppraisalCycle"
 }
 # Document Events
 # ---------------
