517c584a5d83516c90af7db545f5fba565dcb641
ChristineKanga-VF <christinek@victoryfarmskenya.com>
2025-05-13 11:12:58 +0300

    fix: dept appraisals hook

--- victoryfarmsdeveloper/hooks.py
+++ victoryfarmsdeveloper/hooks.py
@@ -127,19 +127,15 @@ doctype_js = {
 # }
 
 permission_query_conditions = {
+    "Appraisal": "victoryfarmsdeveloper.victoryfarmsdeveloper.customization.appraisal.appraisal.get_permission_query_conditions",
     "Department Appraisal": "victoryfarmsdeveloper.victoryfarmsdeveloper.customization.department_appraisal.department_appraisal.get_permission_query_conditions"
+
 }
 
 has_permission = {
+    "Appraisal": "victoryfarmsdeveloper.victoryfarmsdeveloper.customization.appraisal.appraisal.has_permission",
     "Department Appraisal": "victoryfarmsdeveloper.victoryfarmsdeveloper.customization.department_appraisal.department_appraisal.has_permission"
-}
 
-permission_query_conditions = {
-    "Appraisal": "victoryfarmsdeveloper.victoryfarmsdeveloper.customization.appraisal.appraisal.get_permission_query_conditions"
-}
-
-has_permission = {
-    "Appraisal": "victoryfarmsdeveloper.victoryfarmsdeveloper.customization.appraisal.appraisal.has_permission"
 }
 # DocType Class
 # ---------------
