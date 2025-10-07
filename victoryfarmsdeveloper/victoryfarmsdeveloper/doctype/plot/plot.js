4ee7611e07ae32df97b55b156cb28ec33539344d
ChristineKanga-VF <christinek@victoryfarmskenya.com>
2025-10-07 22:26:04 +0300

    novel pt1

---
+++ victoryfarmsdeveloper/victoryfarmsdeveloper/doctype/plot/plot.js
@@ -0,0 +1,39 @@
+// Copyright (c) 2025, Christine K and contributors
+// For license information, please see license.txt
+
+
+frappe.ui.form.on('Plot', {
+    google_map_link: function(frm) {
+        const link = frm.doc.google_map_link;
+        if (!link) return;
+
+        // Try to match Google Maps coordinates pattern
+        // Example: https://www.google.com/maps/place/.../@-0.1234567,34.5678901,17z/
+        const match = link.match(/@(-?\d+\.\d+),(-?\d+\.\d+)/);
+        
+        if (match) {
+            const lat = match[1];
+            const lng = match[2];
+            frm.set_value('lat_location', lat);
+            frm.set_value('long_location', lng);
+            frappe.show_alert({
+                message: __('Latitude and Longitude extracted from Google Maps link.'),
+                indicator: 'green'
+            });
+        } else {
+            frappe.show_alert({
+                message: __('Could not extract coordinates. Check link format.'),
+                indicator: 'red'
+            });
+        }
+    },
+
+    refresh(frm) {
+        if (frm.doc.lat_location && frm.doc.long_location) {
+            frm.add_custom_button(__('View on Map'), () => {
+                const url = `https://www.google.com/maps?q=${frm.doc.lat_location},${frm.doc.long_location}`;
+                window.open(url, '_blank');
+            });
+        }
+    }
+});
