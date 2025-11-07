c07dc368da32cbf8c8eb95ebbd7b7d6809df8d6a
ChristineKanga-VF <christinek@victoryfarmskenya.com>
2025-11-07 10:13:30 +0300

    fix: crop plot list

--- victoryfarmsdeveloper/victoryfarmsdeveloper/doctype/crop/crop.js
+++ victoryfarmsdeveloper/victoryfarmsdeveloper/doctype/crop/crop.js
@@ -1,8 +1,7 @@
-// Copyright (c) 2025, Christine K and contributors
+// Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
+frappe.ui.form.on('Crop', {
+	// refresh: function(frm) {
 
-// frappe.ui.form.on("Crop", {
-// 	refresh(frm) {
-
-// 	},
-// });
+	// }
+});
--- victoryfarmsdeveloper/victoryfarmsdeveloper/doctype/crop/crop.py
+++ victoryfarmsdeveloper/victoryfarmsdeveloper/doctype/crop/crop.py
@@ -1,7 +1,9 @@
-# Copyright (c) 2025, Christine K and contributors
+# Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
