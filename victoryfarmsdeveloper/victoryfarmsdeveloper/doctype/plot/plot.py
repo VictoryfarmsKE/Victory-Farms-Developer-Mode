fdb3099f44d13eac13100d0c483c506b3fa3eefd
ChristineKanga-VF <christinek@victoryfarmskenya.com>
2025-11-14 09:43:14 +0300

    feat: novel update

--- victoryfarmsdeveloper/victoryfarmsdeveloper/doctype/plot/plot.py
+++ victoryfarmsdeveloper/victoryfarmsdeveloper/doctype/plot/plot.py
@@ -9,19 +9,23 @@ import re
 
 
 class Plot(Document):
-    def validate(self):
-        pass
+    # def validate(self):
+    #     pass
 
-@frappe.whitelist()  
-def resolve_google_maps_link(short_url):
-	try:
-		response = requests.head(short_url, allow_redirects=True, timeout=5)
-		final_url = response.url
-		match = re.search(r'@(-?\d+\.\d+),(-?\d+\.\d+)', final_url)
-		if match:
-			return {"lat": match.group(1), "lng": match.group(2)}
-		else:
+	@frappe.whitelist()  
+	def resolve_google_maps_link(short_url):
+		try:
+			response = requests.head(short_url, allow_redirects=True, timeout=5)
+			final_url = response.url
+			match = re.search(r'@(-?\d+\.\d+),(-?\d+\.\d+)', final_url)
+			if match:
+				return {"lat": match.group(1), "lng": match.group(2)}
+			else:
+				return {}
+		except Exception as e:
+			frappe.log_error(frappe.get_traceback(), "Google Maps Resolve Error")
 			return {}
-	except Exception as e:
-		frappe.log_error(frappe.get_traceback(), "Google Maps Resolve Error")
-		return {}
+
+
+	def before_save(self):
+		self.resolve_google_maps_link(self.google_map_link)
