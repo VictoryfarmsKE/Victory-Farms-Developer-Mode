ba591fd9dc448be2adf4c2e3a5542630bd91b499
ChristineKanga-VF <christinek@victoryfarmskenya.com>
2025-11-28 12:53:08 +0300

    update: subcontractor tracker

--- victoryfarmsdeveloper/victoryfarmsdeveloper/report/subcontract_tracker/subcontract_tracker.js
+++ victoryfarmsdeveloper/victoryfarmsdeveloper/report/subcontract_tracker/subcontract_tracker.js
@@ -3,6 +3,20 @@
 
 frappe.query_reports["SubContract Tracker"] = {
 	"filters": [
+		{
+			"fieldname":"purchase_order",
+			"label": __("Purchase Order"),
+			"fieldtype": "Link",
+			"options": "Purchase Order",
+			"width": "80"
+		},
+		{
+			"fieldname":"project",
+			"label": __("Project"),
+			"fieldtype": "Link",
+			"options": "Project",
+			"width": "80"
+		}
 
 	]
 };
