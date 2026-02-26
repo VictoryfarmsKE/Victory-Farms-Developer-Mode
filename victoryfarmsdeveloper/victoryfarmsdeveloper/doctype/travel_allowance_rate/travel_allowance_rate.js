9bbf2185cce7309ecc7f03ff9f7f82628b9176e2
MEryl830 <your.email@example.com>
2026-02-26 16:24:59 +0300

    feat: add Travel Allowance Claim and related documents

---
+++ victoryfarmsdeveloper/victoryfarmsdeveloper/doctype/travel_allowance_rate/travel_allowance_rate.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Christine K and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Travel Allowance Rate", {
+// 	refresh(frm) {
+
+// 	},
+// });
