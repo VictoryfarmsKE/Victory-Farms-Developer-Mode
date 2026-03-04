35c1e499dfb3687f4abf31926e485bf6d9c83355
ChristineKanga-VF <christinek@victoryfarmskenya.com>
2026-03-04 11:31:33 +0300

    feat: remove Leadership Development Card doctype and related files

--- victoryfarmsdeveloper/victoryfarmsdeveloper/doctype/leadership_development_card/test_leadership_development_card.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Christine K and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestLeadershipDevelopmentCard(FrappeTestCase):
-	pass
