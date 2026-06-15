d54beb05d0975d47e2df60845837ad17d41b8511
cynthiaNW <cynthiaw@victoryfarmskenya.com>
2026-06-15 08:40:36 +0300

    Reorder Credit Days and Credit Limit columns in AR Summary Extended to match AP report

--- victoryfarmsdeveloper/victoryfarmsdeveloper/report/accounts_receivable_summary_extended/accounts_receivable_summary_extended.py
+++ victoryfarmsdeveloper/victoryfarmsdeveloper/report/accounts_receivable_summary_extended/accounts_receivable_summary_extended.py
@@ -234,15 +234,15 @@ class AccountsReceivableSummaryExtended(ReceivablePayableReport):
 
 		credit_debit_label = "Credit Note" if self.account_type == "Receivable" else "Debit Note"
 
+		# Credit terms
+		self.add_column(label=_("Credit Days"), fieldname="credit_days", fieldtype="Int", width=100)
+		self.add_column(label=_("Credit Limit"), fieldname="credit_limit", fieldtype="Currency", width=130)
+
 		self.add_column(_("Advance Amount"), fieldname="advance")
 		self.add_column(_("Invoiced Amount"), fieldname="invoiced")
 		self.add_column(_("Paid Amount"), fieldname="paid")
 		self.add_column(_(credit_debit_label), fieldname="credit_note")
 		self.add_column(_("Outstanding Amount"), fieldname="outstanding")
-
-		# Credit terms
-		self.add_column(label=_("Credit Days"), fieldname="credit_days", fieldtype="Int", width=100)
-		self.add_column(label=_("Credit Limit"), fieldname="credit_limit", fieldtype="Currency", width=130)
 
 		if self.filters.show_gl_balance:
 			self.add_column(_("GL Balance"), fieldname="gl_balance")
