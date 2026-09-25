3f6f7fe6cbff2f8209839c0c6588f39e7ccc7f3b
Kelvin Njagi <kelvin.njagi@victoryfarmskenya.com>
2026-09-25 22:59:38 +0300

    fixed syntax error

--- victoryfarmsdeveloper/hooks.py
+++ victoryfarmsdeveloper/hooks.py
@@ -293,7 +293,8 @@ doc_events = {
         "on_update": "victoryfarmsdeveloper.victoryfarmsdeveloper.customization.salary_slip.salary_slip.sync_additional_salary_notes",
         "on_submit": "victoryfarmsdeveloper.victoryfarmsdeveloper.customization.salary_slip.salary_slip.create_journal_entry_on_submit",
         "on_cancel": "victoryfarmsdeveloper.victoryfarmsdeveloper.customization.salary_slip.salary_slip.unlink_journal_entry_on_cancel"
-       },
+    }
+}
 
 
 # Testing
