e8a964b24e3523a0561e16d3f36d72c96cb989fb
Kelvin Njagi <kelvin.njagi@victoryfarmskenya.com>
2026-09-25 23:29:18 +0300

    Add Pending Purchase Order Report

---
+++ victoryfarmsdeveloper/victoryfarmsdeveloper/report/pending_purchase_order_report/pending_purchase_order_report.js
@@ -0,0 +1,35 @@
+// Copyright (c) 2025, Christine K and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Pending Purchase Order Report"] = {
+    filters: [
+        {
+            fieldname: "department",
+            label: __("Department"),
+            fieldtype: "Link",
+            options: "Department"
+        },
+        {
+            fieldname: "cost_center",
+            label: __("Cost Centre"),
+            fieldtype: "Link",
+            options: "Cost Center"
+        },
+        {
+            fieldname: "requester",
+            label: __("Requester"),
+            fieldtype: "Link",
+            options: "User"
+        },
+
+        {
+            fieldname: "created_by",
+            label: __("Created By"),
+            fieldtype: "MultiSelectList",
+            get_data: function(txt) {
+                return frappe.db.get_link_options("User", txt);
+                }
+        },
+    ]
+};
